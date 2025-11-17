907c2f98e5398d10ac4957c70cd998ae290168c3
Muturi John <muturijohn1902@gmail.com>
2025-11-17 18:51:08 +0300

    A simple stack data structure using python list

--- stack.py
+++ stack.py
@@ -1,28 +1,28 @@
-
+# A stack data structure using a python list
 class stack:
     def __init__(self):
         self.stack = []
     
-    def push(self, value):
+    def push(self, value):# add new data
         self.stack.insert(0, value)
 
-    def pop(self):
+    def pop(self):#return and remove the top element
         if len(self.stack) == 0:
             return "empty stack"
         else:
             return self.stack.pop(0)
 
-    def peek(self):
+    def peek(self):#return the top element
         if len(self.stack) == 0:
             return "empty stack"
         else:
             return self.stack[0]
         
-    def isEmpty(self):
+    def isEmpty(self):# Return true if the stack is empty
         if len(self.stack) == 0:
             return True
         else:
             return False
         
-    def size(self):
+    def size(self):#return the size of the stack
         return len(self.stack)

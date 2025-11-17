6c99446eec80d5594ff5aad5e2cf5f31e27efec3
Muturi John <muturijohn1902@gmail.com>
2025-11-17 18:49:56 +0300

    A simple queue data structure using python list

--- queues.py
+++ queues.py
@@ -1,28 +1,29 @@
-
+#A simple queue using python list
 class queue:
     def __init__(self):
         self.queue = []
     
-    def enqueu(self, value):
+    def enqueu(self, value):# add an element at the bottom/end of the queue
         self.queue.append(value)
 
-    def dequeue(self):
+    def dequeue(self):#return and remove the first element of the queue
         if len(self.queue) == 0:
             return "empty queue"
         else:
             return self.queue.pop(0)
         
-    def peek(self):
+    def peek(self):# return the first element of the queue
         if len(self.queue) == 0:
             return "empty queue"
         else:
             return self.queue[0]
         
-    def isEmpty(self):
+    def isEmpty(self):#return true if the list is empty
         if len(self.queue) == 0:
             return True
         else:
             return False
-    def size(self):
+        
+    def size(self):#The size of the queue
         return len(self.queue)
     
